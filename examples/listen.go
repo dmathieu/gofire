38a43a38f88ce155eada4f3bb18e5dfd6071f330
Damien Mathieu <42@dmathieu.com>
2014-01-29 18:09:07 +0100

    use an integer in the listen example

--- examples/listen.go
+++ examples/listen.go
@@ -8,7 +8,7 @@ import (
 func streaming() {
 
 	client := gofire.NewClient("<your API token>", "<your subdomain>", true)
-	room := client.NewRoom("<your room id (not it's name)>")
+	room := client.NewRoom(15) //15 being your room id (not it's name)>
 
 	channel := room.Listen()
 	for {
